d91dd249a56dd33d2439c12e9ba63d71e8a0e281
kakine <kakine@nocdata2-1.(none)>
2014-11-13 14:17:29 +0900

    テスト整理

--- src/test/src/config_test.c
+++ src/test/src/config_test.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#include "bson/bson.h"
+#include "bson.h"
 #include "common_macro.h"
+#include "logger.h"
 #include "bson_helper.h"
 #include "config_manager.h"
 
@@ -19,29 +20,33 @@ static int group_foreach_cb(void *foreach_arg, const char *path, bson_iterator *
         bson_type = bson_iterator_type(itr);
 	ASSERT(bson_type == BSON_OBJECT);
 	key = bson_iterator_key(itr);
-        printf("group.%s\n", key);
-	ASSERT(bson_helper_itr_get_long(itr, &l, "maxForwardRecords", config, "defaultMaxForwardRecords") == 0);
-        ASSERT(l == 3);
-	ASSERT(bson_helper_itr_get_long(itr, &l, "maxReverseRecords", config, "defaultMaxReverseRecords") == 0);
-        ASSERT(l == 1);
-	snprintf(p, sizeof(p), "%s.maxForwardRecords", key);
-	ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "defaultMaxForwardRecords") == 0);
-        ASSERT(l == 3);
-        printf("group.%s.maxForwardRecords - %lld\n", key, (long long)l);
-	snprintf(p, sizeof(p), "%s.maxReverseRecords", key);
-	ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "defaultMaxReverseRecords") == 0);
-        ASSERT(l == 1);
-        printf("group.%s.maxReverseRecords - %lld\n", key, (long long)l);
-	snprintf(p, sizeof(p), "%s.recordSelectAlgorithm", key);
-	ASSERT(bson_helper_itr_get_string(itr, &s, p, NULL, NULL) == 0);
-        ASSERT(strcmp(s, "ransom"));
-        printf("group.%s.recoedSelectAlgorithm - %s\n", key, s);
-	snprintf(p, sizeof(p), "%s.recordPreempt", key);
-	ASSERT(bson_helper_itr_get_bool(itr, &b, p, config, "defaultRecordPreempt") == 0);
-        ASSERT(b == 1);
-        printf("group.%s.recoedPreempt - %d\n", key ,b);
-	snprintf(p, sizeof(p), "%s.NGgroupPriority", key);
-	ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "NGdefaultGroupPriority") == 1);
+	if (strcmp(key, "group0") == 0) {
+		snprintf(p, sizeof(p), "%s.maxForwardRecords", key);
+		ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "defaultMaxForwardRecords") == 0);
+		ASSERT(l == 3);
+		ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "NGdefaultMaxForwardRecords") == 0);
+		ASSERT(l == 3);
+		snprintf(p, sizeof(p), "%s.maxReverseRecords", key);
+		ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "defaultMaxReverseRecords") == 0);
+		ASSERT(l == 3);
+		snprintf(p, sizeof(p), "%s.NGmaxReverseRecords", key);
+		ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "defaultMaxReverseRecords") == 0);
+		ASSERT(l == 5);
+		snprintf(p, sizeof(p), "%s.maxCanonicalNameRecords", key);
+		ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "defaultMaxCanonicalNameRecords") == 0);
+		ASSERT(l == 3);
+		snprintf(p, sizeof(p), "%s.maxForwardRecords", key);
+		ASSERT(bson_helper_itr_get_long(itr, &l, p, NULL, NULL) == 0);
+		ASSERT(l == 3);
+		snprintf(p, sizeof(p), "%s.recordSelectAlgorithm", key);
+		ASSERT(bson_helper_itr_get_string(itr, &s, p, NULL, NULL) == 0);
+		ASSERT(strcmp(s, "roundRobin") == 0);
+		snprintf(p, sizeof(p), "%s.recordPreempt", key);
+		ASSERT(bson_helper_itr_get_bool(itr, &b, p, NULL, NULL) == 0);
+		ASSERT(b == 1);
+		snprintf(p, sizeof(p), "%s.NGgroupPriority", key);
+		ASSERT(bson_helper_itr_get_long(itr, &l, p, config, "NGdefaultGroupPriority") == 1);
+	}
 
 	return BSON_HELPER_FOREACH_SUCCESS;
 }
@@ -53,16 +58,23 @@ main(int argc, char*argv[])
 	bson *config;
 	bson_iterator itr;
 	const char *s;
+	int64_t l;
+	int i;
 
+	ASSERT(logger_create() == 0);
 	ASSERT(config_manager_create(&config_manager) == 0);
-	ASSERT(config_manager_load(config_manager, "../primwatch.conf") == 0);
+	ASSERT(config_manager_load(config_manager, argv[1]) == 0);
 	ASSERT(config_manager_dump(config_manager) == 0);
-	ASSERT(config_manager_load(config_manager, "../primwatch.conf") == 0);
+	ASSERT(config_manager_load(config_manager, argv[1]) == 0);
 	ASSERT(config_manager_dump(config_manager) == 0);
 	ASSERT(config_manager_get_config(config_manager, &config) == 0);
 	ASSERT(bson_helper_bson_get_itr(&itr, config, "groups") == 0);
-	ASSERT(bson_helper_bson_get_string(config, &s, "defaultRecordSelectAlgorithm", NULL) == 0);
-	ASSERT(strcmp(s, "random") == 0);
+	ASSERT(bson_helper_bson_get_long(config, &l, "maxSharedBufferSize", NULL) == 0);
+	ASSERT(l == 67108864);
+	ASSERT(bson_helper_bson_get_string(config, &s, "logType", NULL) == 0);
+	ASSERT(strcmp(s, "syslog") == 0);
+	ASSERT(bson_helper_bson_get_bool(config, &i, "groupPreempt", NULL) == 0);
+	ASSERT(i == 1);
 	ASSERT(bson_helper_bson_get_string(config, &s, "groupSelectOrder", "logType") == 0);
 	ASSERT(strcmp(s, "domainRemoteAddress") == 0);
 	ASSERT(bson_helper_bson_get_string(config, &s, "NGlogType", "logType") == 0);
@@ -70,7 +82,6 @@ main(int argc, char*argv[])
 	ASSERT(bson_helper_bson_get_string(config, &s, "NGlogType", "NGlogType") == 1);
 	ASSERT(bson_helper_itr_foreach(&itr, ".", group_foreach_cb, config) == 0);
 	ASSERT(config_manager_destroy(config_manager) == 0);
-	printf("success\n");
 
 	return 0;
 }
